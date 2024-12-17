befdebf6b6313ae6dfee9299f4b0e32a1251d31c
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2024-12-17 01:47:18 -0600

    fix formatting

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,26 @@
-import {
-  Pressable,
-} from "react-native";
+import { Pressable } from "react-native";
 import { ComponentProps } from "react";
 
 export interface Question {
-    title: string;
-    correctAnswer: string;
-    options: string[];
-  }
-  export interface QuestionCardProps {
-    question: Question;
-  }
+  title: string;
+  correctAnswer: string;
+  options: string[];
+}
+export interface QuestionCardProps {
+  question: Question;
+}
 
-  export interface AnswerOptionProps {
-    option: string;
-    isSelected?: boolean;
-    onPress: () => void;
-  }
-  export interface CardProps {
-    title: string;
-  }
+export interface AnswerOptionProps {
+  option: string;
+  isSelected?: boolean;
+  onPress: () => void;
+}
+export interface CardProps {
+  title: string;
+}
 
-  export interface CustomButtonProps extends ComponentProps<typeof Pressable> {
-    title: string;
-    leftIcon?: React.ReactNode;
-    rightIcon?: React.ReactNode;
-  }
+export interface CustomButtonProps extends ComponentProps<typeof Pressable> {
+  title: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+}
